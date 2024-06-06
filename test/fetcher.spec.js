8876ec4dd25dcec741f67f0c88c512d42ae93836
Alan Shaw <alan.shaw@protocol.ai>
2024-06-06 19:02:25 +0100

    feat: use index claim (#10)
    This PR makes use of the `assert/index` content claim, allowing a [w3-index](https://github.com/w3s-project/specs/blob/main/w3-index.md) to be used to help serve the data, which yields the following perf improvements to the batching blob fetcher:
    
    | Export 500MiB DAG | Sub-requests | Time |
    | -- | -- | -- |
    | without index |  535 | 11.960s |
    | with index | 37 | 7.554s |

--- test/fetcher.spec.js
+++ test/fetcher.spec.js
@@ -4,31 +4,41 @@ import * as Link from 'multiformats/link'
 import * as raw from 'multiformats/codecs/raw'
 import { sha256 } from 'multiformats/hashes/sha2'
 import { equals } from 'multiformats/bytes'
+import { ShardedDAGIndex } from '@web3-storage/blob-index'
 import { fromShardArchives } from '@web3-storage/blob-index/util'
 import * as UnixFS from '@ipld/unixfs'
 import { CARWriterStream } from 'carstream'
 import { exporter } from 'ipfs-unixfs-exporter'
+import * as ed25519 from '@ucanto/principal/ed25519'
 import * as SimpleFetcher from '../src/fetcher/simple.js'
 import * as BatchingFetcher from '../src/fetcher/batching.js'
+import * as ContentClaimsLocator from '../src/locator/content-claims.js'
 import { randomBytes, randomInt } from './helpers/random.js'
 import { concat } from './helpers/stream.js'
 import { settings } from './helpers/unixfs.js'
 import { contentKey, withBucketServer } from './helpers/bucket.js'
+import { generateIndexClaim, generateLocationClaim, generateLocationClaims, withClaimsServer } from './helpers/claims.js'
 import { asBlockstore } from './helpers/unixfs-exporter.js'
-import { patchFetch } from './helpers/fetch.js'
-import { createLocator } from './helpers/locator.js'
+import * as Fetch from './helpers/fetch.js'
+import * as Result from './helpers/result.js'
 
 // simulates cloudflare worker environment with max 6 concurrent reqs
-patchFetch({ concurrency: 6, lag: 50 })
+Fetch.patch({ concurrency: 6, lag: 50 })
 
 export const testFetcher = {}
+
+/**
+ * @typedef {import('./helpers/bucket.js').BucketServerContext & import('./helpers/claims.js').ClaimsServerContext} Context
+ * @param {(assert: import('entail').assert, ctx: Context) => unknown} testfn
+ */
+const withContext = testfn => withBucketServer(withClaimsServer(testfn))
 
 ;[
   { name: 'simple', FetcherFactory: SimpleFetcher },
   { name: 'batching', FetcherFactory: BatchingFetcher }
 ].forEach(({ name, FetcherFactory }) => {
   testFetcher[name] = {
-    'should fetch a file': withBucketServer(async (/** @type {import('entail').assert} assert */ assert, ctx) => {
+    'should fetch a file': withContext(async (/** @type {import('entail').assert} assert */ assert, ctx) => {
       const fileBytes = await randomBytes(10 * 1024 * 1024)
 
       const { readable, writable } = new TransformStream({}, UnixFS.withCapacity(1048576 * 32))
@@ -48,8 +58,14 @@ export const testFetcher = {}
 
       ctx.bucket.put(contentKey(carDigest), carBytes)
 
+      const signer = await ed25519.generate()
       const index = await fromShardArchives(root, [carBytes])
-      const locator = createLocator(carDigest, new URL(contentKey(carDigest), ctx.bucketURL), index)
+      const claims = await generateLocationClaims(signer, new URL(contentKey(carDigest), ctx.bucketURL), index)
+      for (const claim of claims) {
+        ctx.claimsStore.put(claim)
+      }
+
+      const locator = ContentClaimsLocator.create({ serviceURL: ctx.claimsURL })
       const fetcher = FetcherFactory.create(locator)
       const blockstore = asBlockstore(fetcher)
 
@@ -58,7 +74,7 @@ export const testFetcher = {}
       assert.ok(equals(exportedBytes, fileBytes))
     }),
 
-    'should fetch bytes with range': withBucketServer(async (/** @type {import('entail').assert} assert */ assert, ctx) => {
+    'should fetch bytes with range': withContext(async (/** @type {import('entail').assert} assert */ assert, ctx) => {
       const bytes = await randomBytes(1 * 1024 * 1024)
       const root = Link.create(raw.code, await sha256.digest(bytes))
 
@@ -72,8 +88,14 @@ export const testFetcher = {}
       const carBytes = await concat(readable.pipeThrough(new CARWriterStream([root])))
       const carDigest = await sha256.digest(carBytes)
 
+      const signer = await ed25519.generate()
       const index = await fromShardArchives(root, [carBytes])
-      const locator = createLocator(carDigest, new URL(contentKey(carDigest), ctx.bucketURL), index)
+      const claims = await generateLocationClaims(signer, new URL(contentKey(carDigest), ctx.bucketURL), index)
+      for (const claim of claims) {
+        ctx.claimsStore.put(claim)
+      }
+
+      const locator = ContentClaimsLocator.create({ serviceURL: ctx.claimsURL })
       const fetcher = FetcherFactory.create(locator)
 
       ctx.bucket.put(contentKey(carDigest), carBytes)
@@ -81,13 +103,13 @@ export const testFetcher = {}
       const first = randomInt(bytes.length / 2)
       const last = first + randomInt(bytes.length / 2) - 1
       const range = /** @type {API.AbsoluteRange} */ ([first, last])
-      const res = await fetcher.fetch(root.multihash, { range })
+      const blob = Result.unwrap(await fetcher.fetch(root.multihash, { range }))
 
-      assert.ok(res.ok)
-      assert.deepEqual(await res.ok.bytes(), bytes.slice(range[0], range[1] + 1))
+      assert.deepEqual(await blob.bytes(), bytes.slice(range[0], range[1] + 1))
     }),
 
-    'should benchmark 500MB': withBucketServer(async (/** @type {import('entail').assert} assert */ assert, ctx) => {
+    'should benchmark 500MB': withContext(async (/** @type {import('entail').assert} assert */ assert, ctx) => {
+      Fetch.resetCount()
       const fileBytes = await randomBytes(500 * 1024 * 1024)
 
       const { readable, writable } = new TransformStream({}, UnixFS.withCapacity(1048576 * 32))
@@ -107,8 +129,14 @@ export const testFetcher = {}
 
       ctx.bucket.put(contentKey(carDigest), carBytes)
 
+      const signer = await ed25519.generate()
       const index = await fromShardArchives(root, [carBytes])
-      const locator = createLocator(carDigest, new URL(contentKey(carDigest), ctx.bucketURL), index)
+      const claims = await generateLocationClaims(signer, new URL(contentKey(carDigest), ctx.bucketURL), index)
+      for (const claim of claims) {
+        ctx.claimsStore.put(claim)
+      }
+
+      const locator = ContentClaimsLocator.create({ serviceURL: ctx.claimsURL })
       const fetcher = FetcherFactory.create(locator)
       const blockstore = asBlockstore(fetcher)
 
@@ -117,6 +145,56 @@ export const testFetcher = {}
       const exportedBytes = await concat((await exporter(root, blockstore)).content())
       assert.ok(equals(exportedBytes, fileBytes))
       console.timeEnd(`export 500MiB (${name})`)
+      console.log(`sub-requests: ${Fetch.count()}`)
+    }),
+
+    'should benchmark 500MB with index': withContext(async (/** @type {import('entail').assert} assert */ assert, ctx) => {
+      Fetch.resetCount()
+      const fileBytes = await randomBytes(500 * 1024 * 1024)
+
+      const { readable, writable } = new TransformStream({}, UnixFS.withCapacity(1048576 * 32))
+      const writer = UnixFS.createWriter({ writable, settings })
+
+      const [root, carBytes] = await Promise.all([
+        (async () => {
+          const file = UnixFS.createFileWriter(writer)
+          file.write(fileBytes)
+          const { cid } = await file.close()
+          writer.close()
+          return cid
+        })(),
+        concat(readable.pipeThrough(new CARWriterStream()))
+      ])
+      const carDigest = await sha256.digest(carBytes)
+
+      ctx.bucket.put(contentKey(carDigest), carBytes)
+
+      const signer = await ed25519.generate()
+      const index = await fromShardArchives(root, [carBytes])
+      index.shards.get(carDigest)?.set(carDigest, [0, carBytes.length])
+
+      const indexBytes = Result.try(await ShardedDAGIndex.archive(index))
+      const indexDigest = await sha256.digest(indexBytes)
+
+      const blobClaims = await generateLocationClaims(signer, new URL(contentKey(carDigest), ctx.bucketURL), index)
+      for (const claim of blobClaims) {
+        ctx.claimsStore.put(claim)
+      }
+
+      ctx.bucket.put(contentKey(indexDigest), indexBytes)
+      ctx.claimsStore.put(await generateLocationClaim(signer, indexDigest, new URL(contentKey(indexDigest), ctx.bucketURL), 0, indexBytes.length))
+      ctx.claimsStore.put(await generateIndexClaim(signer, root, index))
+
+      const locator = ContentClaimsLocator.create({ serviceURL: ctx.claimsURL })
+      const fetcher = FetcherFactory.create(locator)
+      const blockstore = asBlockstore(fetcher)
+
+      console.time(`export 500MiB with index (${name})`)
+      // @ts-expect-error exporter expects instance not interface
+      const exportedBytes = await concat((await exporter(root, blockstore)).content())
+      assert.ok(equals(exportedBytes, fileBytes))
+      console.timeEnd(`export 500MiB with index (${name})`)
+      console.log(`sub-requests: ${Fetch.count()}`)
     })
   }
 })
---
+++ test/helpers/claims.js
@@ -0,0 +1,150 @@
+// eslint-disable-next-line
+import * as API from '../../src/api.js'
+import http from 'node:http'
+import { Writable } from 'node:stream'
+import { walkClaims } from '@web3-storage/content-claims/server'
+import { DigestMap, ShardedDAGIndex } from '@web3-storage/blob-index'
+import * as Digest from 'multiformats/hashes/digest'
+import { base58btc } from 'multiformats/bases/base58'
+import { CARWriterStream, code as carCode } from 'carstream'
+import { sha256 } from 'multiformats/hashes/sha2'
+import * as Link from 'multiformats/link'
+import { Assert } from '@web3-storage/content-claims/capability'
+
+/**
+ * @typedef {import('@web3-storage/content-claims/server/api').ClaimFetcher} ClaimFetcher
+ * @typedef {{ claimsStore: ClaimStorage, claimsURL: URL }} ClaimsServerContext
+ */
+
+/**
+ * @template {{}} T
+ * @param {(assert: import('entail').assert, ctx: T & ClaimsServerContext) => unknown} testfn
+ */
+export const withClaimsServer = testfn =>
+  /** @type {(assert: import('entail').assert, ctx: T) => unknown} */
+  // eslint-disable-next-line no-extra-parens
+  (async (assert, ctx) => {
+    const claimsStore = new ClaimStorage()
+    const server = http.createServer((req, res) => {
+      const url = new URL(req.url ?? '', claimsURL)
+      const digest = Digest.decode(base58btc.decode(url.pathname.split('/')[3]))
+      walkClaims({ claimFetcher: claimsStore }, digest, new Set())
+        .pipeThrough(new CARWriterStream())
+        .pipeTo(Writable.toWeb(res))
+    })
+    await new Promise(resolve => server.listen(resolve))
+    // @ts-expect-error
+    const { port } = server.address()
+    const claimsURL = new URL(`http://127.0.0.1:${port}`)
+    try {
+      await testfn(assert, { ...ctx, claimsStore, claimsURL })
+    } finally {
+      server.close()
+    }
+  })
+
+/** @implements {ClaimFetcher} */
+class ClaimStorage {
+  constructor () {
+    /** @type {Map<API.MultihashDigest, import('@web3-storage/content-claims/server/api').Claim[]>} */
+    this.data = new DigestMap()
+  }
+
+  /** @param {import('@web3-storage/content-claims/server/api').Claim} claim */
+  async put (claim) {
+    const claims = this.data.get(claim.content) ?? []
+    claims.push(claim)
+    this.data.set(claim.content, claims)
+  }
+
+  /** @param {API.MultihashDigest} content */
+  async get (content) {
+    return this.data.get(content) ?? []
+  }
+}
+
+/**
+ * @param {import('@ucanto/interface').Signer} signer
+ * @param {URL} location
+ * @param {import('@web3-storage/blob-index/types').ShardedDAGIndex} index
+ */
+export const generateLocationClaims = async (signer, location, index) => {
+  /** @type {import('@web3-storage/content-claims/server/api').Claim[]} */
+  const claims = []
+  for (const [, slices] of index.shards) {
+    for (const [slice, pos] of slices) {
+      claims.push(await generateLocationClaim(signer, slice, location, pos[0], pos[1]))
+    }
+  }
+  return claims
+}
+
+/**
+ * @param {import('@ucanto/interface').Signer} signer
+ * @param {API.MultihashDigest} digest
+ * @param {URL} location
+ * @param {number} offset
+ * @param {number} length
+ */
+export const generateLocationClaim = async (signer, digest, location, offset, length) => {
+  const invocation = Assert.location.invoke({
+    issuer: signer,
+    audience: signer,
+    with: signer.did(),
+    nb: {
+      content: { digest: digest.bytes },
+      location: [
+        /** @type {API.URI} */
+        (location.toString())
+      ],
+      range: { offset, length }
+    }
+  })
+  const block = await encode(invocation)
+  return {
+    claim: block.cid,
+    bytes: block.bytes,
+    content: digest,
+    value: block.value
+  }
+}
+
+/**
+ * Encode a claim to a block.
+ * @param {import('@ucanto/interface').IssuedInvocation<import('@web3-storage/content-claims/server/api').AnyAssertCap>} invocation
+ */
+const encode = async invocation => {
+  const view = await invocation.buildIPLDView()
+  const bytes = await view.archive()
+  if (bytes.error) throw new Error('failed to archive')
+  return {
+    cid: Link.create(carCode, await sha256.digest(bytes.ok)),
+    bytes: bytes.ok,
+    value: invocation.capabilities[0]
+  }
+}
+
+/**
+ * @param {import('@ucanto/interface').Signer} signer
+ * @param {import('multiformats').UnknownLink} content
+ * @param {import('@web3-storage/blob-index/types').ShardedDAGIndex} index
+ */
+export const generateIndexClaim = async (signer, content, index) => {
+  const res = await ShardedDAGIndex.archive(index)
+  if (!res.ok) throw new Error('failed to archive')
+
+  const indexLink = Link.create(carCode, await sha256.digest(res.ok))
+  const invocation = Assert.index.invoke({
+    issuer: signer,
+    audience: signer,
+    with: signer.did(),
+    nb: { content, index: indexLink }
+  })
+  const block = await encode(invocation)
+  return {
+    claim: block.cid,
+    bytes: block.bytes,
+    content: content.multihash,
+    value: block.value
+  }
+}
